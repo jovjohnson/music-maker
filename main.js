c01fe99204f4913022f733869dd02f38b007357f
Jovvi Johnson <jjohnsow@gmail.com>
2016-06-06 23:21:11 -0500

    clean up

--- main.js
+++ main.js
@@ -2,52 +2,47 @@
 
 $(document).ready(function() {
 
-var c = document.getElementById('c-note');
-var d = document.getElementById('d-note');
-var e = document.getElementById('e-note');
-var f = document.getElementById('f-note');
-var g = document.getElementById('g-note');
-var a = document.getElementById('a-note');
-var b = document.getElementById('b-note');
+  var c = document.getElementById('c-note');
+  var d = document.getElementById('d-note');
+  var e = document.getElementById('e-note');
+  var f = document.getElementById('f-note');
+  var g = document.getElementById('g-note');
+  var a = document.getElementById('a-note');
+  var b = document.getElementById('b-note');
 
-$('#c').mouseover(function() {
-  c.currentTime = 0;
-  c.play();
-});
+  $('#c').mouseover(function() {
+    c.currentTime = 0;
+    c.play();
+  });
 
-$('#d').mouseover(function() {
-  d.currentTime = 0;
-  d.play();
-});
+  $('#d').mouseover(function() {
+    d.currentTime = 0;
+    d.play();
+  });
 
-$('#e').mouseover(function() {
-  e.currentTime = 0;
-  e.play();
-});
+  $('#e').mouseover(function() {
+    e.currentTime = 0;
+    e.play();
+  });
 
-$('#f').mouseover(function() {
-  f.currentTime = 0;
-  f.play();
-});
+  $('#f').mouseover(function() {
+    f.currentTime = 0;
+    f.play();
+  });
 
-$('#g').mouseover(function() {
-  g.currentTime = 0;
-  g.play();
-});
+  $('#g').mouseover(function() {
+    g.currentTime = 0;
+    g.play();
+  });
 
-$('#a').mouseover(function() {
-  a.currentTime = 0;
-  a.play();
-});
+  $('#a').mouseover(function() {
+    a.currentTime = 0;
+    a.play();
+  });
 
-$('#b').mouseover(function() {
-  b.currentTime = 0;
-  b.play();
-});
-
-
-
-
-
-
+  $('#b').mouseover(function() {
+    b.currentTime = 0;
+    b.play();
+  });
+  
 });
